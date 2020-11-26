15069f1ef2cf12b3d797d69daea28d20ee4a0788
Numan Ahmed <mdnmnahmed@gmail.com>
2020-11-27 01:41:25 +0530

    Create Post route created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,6 +9,15 @@ export const getPosts = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
-    res.send('Post Creation');
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostModel(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
 }
