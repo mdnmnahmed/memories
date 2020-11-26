c716875bce7927db048ba6906aaaeb38599618c3
Numan Ahmed <mdnmnahmed@gmail.com>
2020-11-26 20:10:40 +0530

    Database connection with server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,27 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const MONGODB_URL = 'mongodb+srv://namazon:namazon@cluster0.lro23.mongodb.net/namazondb?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then((res) => {
+        console.log("Connected with MongoDB ");
+    })
+    .catch((err) => {
+        console.log("Error in with MongoDB ", err);
+    });
+
+mongoose.set('useFindAndModify', false); //To prevent warnings at console
+
+app.listen(PORT, () => {
+    console.log('App Started at Port: ', PORT);
+});
